4752c49780de1afaf8b3da444ea96118a4ec23cb
Yandi Banyu Karima Waly <yandibanyukarimawaly@gmail.com>
2019-10-17 04:07:02 +0700

    Memulai refactoring. Prinsip reusable code.

--- remote/build.sh
+++ remote/build.sh
@@ -2,8 +2,10 @@
 
 source /opt/ros/kinetic/setup.bash
 source ~/eros_ws_1/install/setup.bash
-echo -e '\E[00;35m'"\033[1m[krsbi] Bismillah KRSBI Juara 1.\033[0m"
-echo -e '\E[00;35m'"\033[1m[krsbi] Running catkin build.\033[0m"
+
+echo -e "\033[1m[krsbi] Bismillah KRSBI Juara 1.\033[0m"
+echo -e "\033[1m[krsbi] Running catkin build.\033[0m"
+
 x=$1
 cd ~/eros_ws_1/
 catkin build $x --cmake-args -DCMAKE_BUILD_TYPE=Release
--- robocup/build.sh
+++ robocup/build.sh
@@ -2,8 +2,10 @@
 
 source /opt/ros/kinetic/setup.bash
 source ~/eros_ws_robocup/install/setup.bash
-echo -e '\E[00;35m'"\033[1m[robocup] Bismillah ROBOCUP Juara 1.\033[0m"
-echo -e '\E[00;35m'"\033[1m[robocup] Running catkin build.\033[0m"
+
+echo -e "\033[1m[robocup] Bismillah ROBOCUP Juara 1.\033[0m"
+echo -e "\033[1m[robocup] Running catkin build.\033[0m"
+
 x=$1
 cd ~/eros_ws_robocup/
 catkin build $x --cmake-args -DCMAKE_BUILD_TYPE=Release
